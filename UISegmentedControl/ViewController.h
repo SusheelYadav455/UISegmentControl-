df0fcbbab257c73fdb50695b8c0b2ed014e94516
susheel <susheel455@gmail.com>
2016-06-22 17:38:09 +0530

    Initial Commit

---
+++ UISegmentedControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UISegmentedControl
+//
+//  Created by Ajeet N on 22/06/16.
+//  Copyright © 2016 AhexTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
